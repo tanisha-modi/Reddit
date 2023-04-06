b7cf4af33456693fc4fb349c470c2b273d3d3f53
tanisha <tanishamodi12@gmail.com>
2023-04-06 14:38:59 +0530

    final

---
+++ Redit/src/components/Modal/Auth/index.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import {
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
+import { userState } from "../../../atoms/userAtom";
+import { auth } from "../../../firebase/clientApp";
+import AuthInputs from "./Inputs";
+import OAuthButtons from "./OAuthButtons";
+import ResetPassword from "./ResetPassword";
+import ModalWrapper from "../ModalWrapper";
+
+type AuthModalProps = {
+  children?: React.ReactNode | undefined
+};
+
+const AuthModal: React.FC<AuthModalProps> = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+  const handleClose = () =>
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+
+  const currentUser = useRecoilValue(userState);
+  const [user, error] = useAuthState(auth);
+
+  // Can implement at the end
+  // useEffect(() => {
+  //   if (currentUser) handleClose();
+  // }, [currentUser]);
+  const toggleView = (view: string) => {
+    setModalState({
+      ...modalState,
+      view: view as typeof modalState.view,
+    });
+  };
+
+  useEffect(() => {
+    if (user) handleClose();
+  }, [user]);
+
+  return (
+    <ModalWrapper isOpen={modalState.open} onClose={handleClose}>
+      <ModalHeader display="flex" flexDirection="column" alignItems="center">
+        {modalState.view === "login" && "Login"}
+        {modalState.view === "signup" && "Sign Up"}
+        {modalState.view === "resetPassword" && "Reset Password"}
+      </ModalHeader>
+      <ModalCloseButton />
+      <ModalBody
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        pb={6}
+      >
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          width="70%"
+        >
+          {modalState.view === "login" || modalState.view === "signup" ? (
+            <>
+              <OAuthButtons />
+              OR
+              <AuthInputs toggleView={toggleView} />
+            </>
+          ) : (
+            <ResetPassword toggleView={toggleView} />
+          )}
+          
+     
+        </Flex>
+      </ModalBody>
+    </ModalWrapper>
+  );
+};
+export default AuthModal;
